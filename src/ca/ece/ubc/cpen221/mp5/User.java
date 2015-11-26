f76e669b5f36b7dfea7f893dc5ada4f92407527a
Erik MacLennan <erikmaclennan@alumni.ubc.ca>
2015-11-25 21:22:04 -0800

    Preparing the json parsing and creating all fields for the restaurant, review, and user object

--- src/ca/ece/ubc/cpen221/mp5/User.java
+++ src/ca/ece/ubc/cpen221/mp5/User.java
@@ -3,5 +3,20 @@ package ca.ece.ubc.cpen221.mp5;
 // TODO: Use this class to represent a Yelp user.
 
 public class User {
-
+    private String url;
+    private int[] votes;
+    private int reviewCount;
+    private String userID;
+    private String name;
+    private double averageStars;
+    
+    
+    public User(String url,
+                 int[] votes,
+                 int reviewCount,
+                 String userID,
+                 String name,
+                 double averageStars){
+        
+    }
 }

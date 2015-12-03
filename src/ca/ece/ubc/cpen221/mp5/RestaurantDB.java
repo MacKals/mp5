fc56a038b567cdcea7dab5d175703a573a210267
Erik MacLennan <erikmaclennan@alumni.ubc.ca>
2015-12-02 22:36:25 -0800

    created the addUser, restaurant, etc methods

--- src/ca/ece/ubc/cpen221/mp5/RestaurantDB.java
+++ src/ca/ece/ubc/cpen221/mp5/RestaurantDB.java
@@ -3,6 +3,8 @@ package ca.ece.ubc.cpen221.mp5;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
+import com.sun.javafx.scene.paint.GradientUtils.Parser;
+
 import ca.ece.ubc.cpen221.mp5.query.QueryFactory;
 
 import org.json.simple.parser.JSONParser;
@@ -46,9 +48,9 @@ public class RestaurantDB {
      */
     public RestaurantDB(String restaurantJSONfilename, String reviewsJSONfilename, String usersJSONfilename) {
 
-        restaurants = addFromFile(restaurantJSONfilename, FileKind.Restaurant);
-        reviews = addFromFile(reviewsJSONfilename, FileKind.Review);
-        users = addFromFile(usersJSONfilename, FileKind.User);
+        addFromFile(restaurantJSONfilename, FileKind.Restaurant);
+        addFromFile(reviewsJSONfilename, FileKind.Review);
+        addFromFile(usersJSONfilename, FileKind.User);
 
     }
 
@@ -65,7 +67,7 @@ public class RestaurantDB {
     }
 
     /**
-     * // Write specs, etc.
+     * // Write specs, etc.MORTEN WRITE THIS SPEC PLEASE LOL.
      * 
      * @param queryString
      * @return
@@ -78,110 +80,147 @@ public class RestaurantDB {
         Restaurant, Review, User;
     }
 
-    private ArrayList<Object> addFromFile(String filename, FileKind fileKind) {
+    /** Adds all objects, be it restaurants, reviews, or users from a file to the RestaurantDB
+     * @param filename the name of the JSON file, including the .json file extension. file must be within the "data" folder.
+     * @param fileKind The type of file we wish to add to our database
+     * @return true if the objects were successfully added from the file.  
+     */
+    private boolean addFromFile(String filename, FileKind fileKind) {
 
-        ArrayList<Object> returnList = new ArrayList<Object>();
-        JSONParser parser = new JSONParser();
-        Object obj = new Object();
 
         try {
             Iterator<String> fileEntries = new BufferedReader(new FileReader("data/" + filename)).lines().iterator();
 
             while (fileEntries.hasNext()) {
                 
-                obj = parser.parse(fileEntries.next());
+                
                 
                 if (fileKind == FileKind.Restaurant) {
 
                     System.out.println("reading a restaurant");
 
-                    JSONObject restaurant = (JSONObject) obj;
-
-                    Object newRestaurant = new Restaurant((String) restaurant.get("url"),
-                            (String) restaurant.get("photo_url"), (double) restaurant.get("longitude"),
-                            (double) restaurant.get("latitude"), (String) restaurant.get("city"),
-                            (String) restaurant.get("full_address"), (ArrayList<String>) restaurant.get("neighborhoods"),
-                            (String) restaurant.get("state"), (ArrayList<String>) restaurant.get("schools"),
-                            (String) restaurant.get("name"), (String) restaurant.get("business_id"),
-                            (boolean) restaurant.get("open"), (ArrayList<String>) restaurant.get("categories"),
-                            (double) restaurant.get("stars"),
-                            RestaurantDB.safeLongToInt((long) restaurant.get("review_count")),
-                            RestaurantDB.safeLongToInt((long) restaurant.get("price")));
-                    returnList.add(newRestaurant);
-                    restaurants.add(newRestaurant);
+                    addRestaurant(fileEntries.next());
 
                 } else if (fileKind == FileKind.Review) {
 
                     System.out.println("reading a review");
-                    JSONObject review = (JSONObject) obj;
-                    Object votes = review.get("votes");
-
-                    Object newReview = new Review((String) review.get("business_id"), (String) review.get("user_id"),
-                            (Object) review.get("votes"), (String) review.get("review_id"), (String) review.get("text"),
-                            RestaurantDB.safeLongToInt((long) review.get("stars")), (String) review.get("date"));
-
-                    returnList.add(newReview);
-                    reviews.add(newReview);
+                    
+                    addReview(fileEntries.next());
 
 
                 } else if (fileKind == FileKind.User) {
 
                     System.out.println("reading a user");
-                    JSONObject user = (JSONObject) obj;
-
-                    Object newUser = new User((String) user.get("url"), (Object) user.get("votes"),
-                            RestaurantDB.safeLongToInt((long) user.get("review_count")), (String) user.get("user_id"),
-                            (String) user.get("name"), (double) user.get("average_stars"));
-
-                    returnList.add(newUser);
-                    users.add(newUser);
+                    
+                    addUser(fileEntries.next());
 
                 }
 
                 System.out.println("Now we're here");
                 System.out.println();
             }
-
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        return returnList;
+        
+        
+        return true;
 
     }
 
-
+    /** Converts a long to an int, provided the long can be converted without losing any of its value 
+     * @param l long value we wish to convert to  
+     * @return the int corresponding to the long passed in
+     * @throws IllegalArgumentException if the long cannot fit into the size of an int.
+     */
     private static int safeLongToInt(long l) {
         if (l < Integer.MIN_VALUE || l > Integer.MAX_VALUE) {
             throw new IllegalArgumentException(l + " cannot be cast to int without changing its value.");
         }
         return (int) l;
     }
-
-
     
-    
-    
-    //TODO: implement 
+    /** Adds a restaurant to the restaurantDB 
+     * @param restaurantString a string in JSON format corresponding to a restaurant in the Yelp database.
+     * @return true if restaurant add was successful, false otherwise.
+     */
     public boolean addRestaurant(String restaurantString) {
         
+        JSONParser parser = new JSONParser();
+        Object obj = new Object();
         
+        try {
+            obj = parser.parse(restaurantString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         
-        return false;
+        JSONObject restaurant = (JSONObject) obj;
+
+        Object newRestaurant = new Restaurant((String) restaurant.get("url"),
+                (String) restaurant.get("photo_url"), (double) restaurant.get("longitude"),
+                (double) restaurant.get("latitude"), (String) restaurant.get("city"),
+                (String) restaurant.get("full_address"), (ArrayList<String>) restaurant.get("neighborhoods"),
+                (String) restaurant.get("state"), (ArrayList<String>) restaurant.get("schools"),
+                (String) restaurant.get("name"), (String) restaurant.get("business_id"),
+                (boolean) restaurant.get("open"), (ArrayList<String>) restaurant.get("categories"),
+                (double) restaurant.get("stars"),
+                RestaurantDB.safeLongToInt((long) restaurant.get("review_count")),
+                RestaurantDB.safeLongToInt((long) restaurant.get("price")));
+
+        return restaurants.add(newRestaurant);
     }
     
     
-    //TODO: implement 
+    /** Adds a user to the restaurantDB 
+     * @param usertString a string in JSON format corresponding to a user in the Yelp database.
+     * @return true if user add was successful, false otherwise.
+     */
     public boolean addUser(String userString) {
         
-        return false;
+        JSONParser parser = new JSONParser();
+        Object obj = new Object();
+        
+        try {
+            obj = parser.parse(userString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        JSONObject user = (JSONObject) obj;
+        
+        Object newUser = new User((String) user.get("url"), (Object) user.get("votes"),
+                RestaurantDB.safeLongToInt((long) user.get("review_count")), (String) user.get("user_id"),
+                (String) user.get("name"), (double) user.get("average_stars"));
+
+        return users.add(newUser);
     }
     
     
-    //TODO: implement 
+    /** Adds a review to the restaurantDB 
+     * @param reviewString a string in JSON format corresponding to a review in the Yelp database.
+     * @return true if review add was successful, false otherwise.
+     */ 
     public boolean addReview(String reviewString) {
         
-        return false;
+        JSONParser parser = new JSONParser();
+        Object obj = new Object();
+        
+        try {
+            obj = parser.parse(reviewString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        JSONObject review = (JSONObject) obj;
+        
+
+        Object newReview = new Review((String) review.get("business_id"), (String) review.get("user_id"),
+                (Object) review.get("votes"), (String) review.get("review_id"), (String) review.get("text"),
+                RestaurantDB.safeLongToInt((long) review.get("stars")), (String) review.get("date"));
+
+        return reviews.add(newReview);
     }
     
 

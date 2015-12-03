b851820919de1b91a8f924f5cc9dca573ad7a1ea
Erik MacLennan <erikmaclennan@alumni.ubc.ca>
2015-12-03 10:49:16 -0800

    added some specs and the clusters to JSON

--- src/ca/ece/ubc/cpen221/mp5/RestaurantDB.java
+++ src/ca/ece/ubc/cpen221/mp5/RestaurantDB.java
@@ -70,21 +70,24 @@ public class RestaurantDB {
         Restaurant, Review, User;
     }
 
-    /** Adds all objects, be it restaurants, reviews, or users from a file to the RestaurantDB
-     * @param filename the name of the JSON file, including the .json file extension. file must be within the "data" folder.
-     * @param fileKind The type of file we wish to add to our database
-     * @return true if the objects were successfully added from the file.  
+    /**
+     * Adds all objects, be it restaurants, reviews, or users from a file to the
+     * RestaurantDB
+     * 
+     * @param filename
+     *            the name of the JSON file, including the .json file extension.
+     *            file must be within the "data" folder.
+     * @param fileKind
+     *            The type of file we wish to add to our database
+     * @return true if the objects were successfully added from the file.
      */
     private boolean addFromFile(String filename, FileKind fileKind) {
-
 
         try {
             Iterator<String> fileEntries = new BufferedReader(new FileReader("data/" + filename)).lines().iterator();
 
             while (fileEntries.hasNext()) {
-                
-                
-                
+
                 if (fileKind == FileKind.Restaurant) {
 
                     System.out.println("reading a restaurant");
@@ -94,14 +97,13 @@ public class RestaurantDB {
                 } else if (fileKind == FileKind.Review) {
 
                     System.out.println("reading a review");
-                    
-                    addReview(fileEntries.next());
 
+                    addReview(fileEntries.next());
 
                 } else if (fileKind == FileKind.User) {
 
                     System.out.println("reading a user");
-                    
+
                     addUser(fileEntries.next());
 
                 }
@@ -109,20 +111,24 @@ public class RestaurantDB {
                 System.out.println("Now we're here");
                 System.out.println();
             }
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
-        
+
         return true;
 
     }
 
-    /** Converts a long to an int, provided the long can be converted without losing any of its value 
-     * @param l long value we wish to convert to  
+    /**
+     * Converts a long to an int, provided the long can be converted without
+     * losing any of its value
+     * 
+     * @param l
+     *            long value we wish to convert to
      * @return the int corresponding to the long passed in
-     * @throws IllegalArgumentException if the long cannot fit into the size of an int.
+     * @throws IllegalArgumentException
+     *             if the long cannot fit into the size of an int.
      */
     private static int safeLongToInt(long l) {
         if (l < Integer.MIN_VALUE || l > Integer.MAX_VALUE) {
@@ -130,81 +136,89 @@ public class RestaurantDB {
         }
         return (int) l;
     }
-    
-    /** Adds a restaurant to the restaurantDB 
-     * @param restaurantString a string in JSON format corresponding to a restaurant in the Yelp database.
+
+    /**
+     * Adds a restaurant to the restaurantDB
+     * 
+     * @param restaurantString
+     *            a string in JSON format corresponding to a restaurant in the
+     *            Yelp database.
      * @return true if restaurant add was successful, false otherwise.
      */
     public boolean addRestaurant(String restaurantString) {
-        
+
         JSONParser parser = new JSONParser();
         Object obj = new Object();
-        
+
         try {
             obj = parser.parse(restaurantString);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        
+
         JSONObject restaurant = (JSONObject) obj;
 
-        Object newRestaurant = new Restaurant((String) restaurant.get("url"),
-                (String) restaurant.get("photo_url"), (double) restaurant.get("longitude"),
-                (double) restaurant.get("latitude"), (String) restaurant.get("city"),
-                (String) restaurant.get("full_address"), (ArrayList<String>) restaurant.get("neighborhoods"),
-                (String) restaurant.get("state"), (ArrayList<String>) restaurant.get("schools"),
-                (String) restaurant.get("name"), (String) restaurant.get("business_id"),
-                (boolean) restaurant.get("open"), (ArrayList<String>) restaurant.get("categories"),
-                (double) restaurant.get("stars"),
+        Object newRestaurant = new Restaurant((String) restaurant.get("url"), (String) restaurant.get("photo_url"),
+                (double) restaurant.get("longitude"), (double) restaurant.get("latitude"),
+                (String) restaurant.get("city"), (String) restaurant.get("full_address"),
+                (ArrayList<String>) restaurant.get("neighborhoods"), (String) restaurant.get("state"),
+                (ArrayList<String>) restaurant.get("schools"), (String) restaurant.get("name"),
+                (String) restaurant.get("business_id"), (boolean) restaurant.get("open"),
+                (ArrayList<String>) restaurant.get("categories"), (double) restaurant.get("stars"),
                 RestaurantDB.safeLongToInt((long) restaurant.get("review_count")),
                 RestaurantDB.safeLongToInt((long) restaurant.get("price")));
 
         return restaurants.add(newRestaurant);
     }
-    
-    
-    /** Adds a user to the restaurantDB 
-     * @param usertString a string in JSON format corresponding to a user in the Yelp database.
+
+    /**
+     * Adds a user to the restaurantDB
+     * 
+     * @param usertString
+     *            a string in JSON format corresponding to a user in the Yelp
+     *            database.
      * @return true if user add was successful, false otherwise.
      */
     public boolean addUser(String userString) {
-        
+
         JSONParser parser = new JSONParser();
         Object obj = new Object();
-        
+
         try {
             obj = parser.parse(userString);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        
+
         JSONObject user = (JSONObject) obj;
-        
+
         Object newUser = new User((String) user.get("url"), (Object) user.get("votes"),
                 RestaurantDB.safeLongToInt((long) user.get("review_count")), (String) user.get("user_id"),
                 (String) user.get("name"), (double) user.get("average_stars"));
 
         return users.add(newUser);
     }
-    
-    
-    /** Adds a review to the restaurantDB 
-     * @param reviewString a string in JSON format corresponding to a review in the Yelp database.
+
+    /**
+     * Adds a review to the restaurantDB
+     * 
+     * @param reviewString
+     *            a string in JSON format corresponding to a review in the Yelp
+     *            database.
      * @return true if review add was successful, false otherwise.
-     */ 
+     */
     public boolean addReview(String reviewString) {
-        
+
         JSONParser parser = new JSONParser();
         Object obj = new Object();
-        
+
         try {
             obj = parser.parse(reviewString);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        
+
         JSONObject review = (JSONObject) obj;
-        
 
         Object newReview = new Review((String) review.get("business_id"), (String) review.get("user_id"),
                 (Object) review.get("votes"), (String) review.get("review_id"), (String) review.get("text"),
@@ -212,6 +226,5 @@ public class RestaurantDB {
 
         return reviews.add(newReview);
     }
-    
 
 }
--- src/ca/ece/ubc/cpen221/mp5/statlearning/Algorithms.java
+++ src/ca/ece/ubc/cpen221/mp5/statlearning/Algorithms.java
@@ -1,6 +1,7 @@
 package ca.ece.ubc.cpen221.mp5.statlearning;
 
 import java.util.Set;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import ca.ece.ubc.cpen221.mp5.*;
@@ -8,10 +9,13 @@ import ca.ece.ubc.cpen221.mp5.*;
 public class Algorithms {
 
     /**
-     * Use k-means clustering to compute k clusters for the restaurants in the
-     * database.
+     * Performs a k-means clustering for the restaurants within the database,
+     * based on the restaurants' locations.
      * 
+     * @param k
+     *            the number of clusters desired
      * @param db
+     *            the RestaurantDB containing the restaurants we want to cluster
      * @return
      */
     public static List<Set<Restaurant>> kMeansClustering(int k, RestaurantDB db) {
@@ -99,42 +103,109 @@ public class Algorithms {
         return allClusters;
     }
 
+    /**
+     * Converts a list of restaurant clusters to JSON format.
+     * 
+     * @param clusters
+     *            a list of n sets, where each set represents a cluster of
+     *            restaurants.
+     * @return A string in JSON format of all the restaurants based on their
+     *         clustering
+     */
     public static String convertClustersToJSON(List<Set<Restaurant>> clusters) {
-        // TODO: Implement this method
-        return null;
+        String theString = new String();
+        try {
+            
+            for (Set<Restaurant> set : clusters) {
+                for (Restaurant restaurant : set) {
+
+                    theString += restaurant.representationInJSON();
+
+                }
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return theString;
     }
 
+    /**
+     * Based on a particular feature function, returns a function that predicts
+     * what rating a user will give a restaurant based on this feature function.
+     * These feature function include:
+     * 
+     * restaurant price scale, restaurant mean rating, restaurant location:
+     * latitude, restaurant location: longitude, restaurant category
+     * 
+     * @param u
+     *            The user to be analyzed to predict its ratings.
+     * @param db
+     *            The RestaurantDB containing the restaurants to analyze
+     * @param featureFunction
+     *            a function that returns a property of a particular restaurant.
+     * @return
+     */
     public static MP5Function getPredictor(User u, RestaurantDB db, MP5Function featureFunction) {
 
         ArrayList<Double> regCoefficients = computeRegressionCoefficients(featureFunction, u, db);
 
-        RegressionFunction returnFunction = new RegressionFunction(regCoefficients.get(1), regCoefficients.get(2), featureFunction);
-        
+        RegressionFunction returnFunction = new RegressionFunction(regCoefficients.get(1), regCoefficients.get(2),
+                featureFunction);
+
         return returnFunction;
     }
 
+    /**
+     * From a list of feature functions, returns the function that is the best
+     * predictor of the user. This method returns the feature function resulting
+     * in the highest R^2 value from a linear regression.
+     * 
+     * @param u
+     *            the User to predict ratings of
+     * @param db
+     *            the RestaurantDB
+     * @param featureFunctionList
+     *            a list of MP5Functions to analyze
+     * @return the feature function that best predicts the user's ratings
+     */
     public static MP5Function getBestPredictor(User u, RestaurantDB db, List<MP5Function> featureFunctionList) {
-        
+
         int indexOfGreatestR2 = 0;
         double currentGreatestR2 = 0;
         double r2InQuestion;
-        
-        for (MP5Function featureFunction : featureFunctionList){
-            
+
+        for (MP5Function featureFunction : featureFunctionList) {
+
             r2InQuestion = computeRegressionCoefficients(featureFunction, u, db).get(0);
-            
-            if ( r2InQuestion > currentGreatestR2){ //extract the r-squared from the returned list
+
+            if (r2InQuestion > currentGreatestR2) { // extract the r-squared
+                                                    // from the returned list
                 currentGreatestR2 = r2InQuestion;
                 indexOfGreatestR2 = featureFunctionList.indexOf(featureFunction);
             }
         }
-        
+
         return featureFunctionList.get(indexOfGreatestR2);
-        
+
     }
 
-    private static ArrayList<Double> computeRegressionCoefficients(MP5Function featureFunction, User u, RestaurantDB db) { //list of R2, a ,b in that order
-        
+    /**
+     * computes the regression coefficients, R^2, a, b, and returns a list of
+     * these values in this order.
+     * 
+     * @param featureFunction
+     * @param u
+     *            the User
+     * @param db
+     *            the RestaurantDB
+     * @return an ArrayList containing 3 doubles, in the order R^2, a, b.
+     */
+    private static ArrayList<Double> computeRegressionCoefficients(MP5Function featureFunction, User u,
+            RestaurantDB db) { // list of r_squared, a ,b in that order
+
         double sumOfInput = 0;
         double sumOfOutput = 0;
 
@@ -186,20 +257,28 @@ public class Algorithms {
             syy += (outputs.get(i) - meanOutput) * (outputs.get(i) - meanOutput);
             sxy += (inputs.get(i) - meanInput) * (outputs.get(i) - meanOutput);
         }
-        
-        
-        
-        double b = sxy/sxx;
-        
+
+        double b = sxy / sxx;
+
         ArrayList<Double> returnList = new ArrayList<Double>();
         returnList.add((sxy * sxy) / (sxx * syy));
         returnList.add(meanOutput - b * meanInput);
         returnList.add(b);
-        
-        return returnList ;
-        
+
+        return returnList;
+
     }
 
+    /**
+     * From a set of restaurants, computes the coordinate that is the centroid
+     * of all the restaurants in the set.
+     * 
+     * @param coordinates
+     *            set of Restaurants
+     * @return a Coordinate that corresponds to the centroid of the cluster of
+     *         restaurants
+     * 
+     */
     private static Coordinate computeCentroidOfRestaurants(Set<Restaurant> coordinates) {
 
         double newX = 0;

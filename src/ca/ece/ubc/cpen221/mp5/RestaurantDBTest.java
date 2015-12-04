2e7475fc1cdcbcc9f64aff523b6893ed19332445
Erik MacLennan <erikmaclennan@alumni.ubc.ca>
2015-12-04 11:18:34 -0800

    writing file voronoi.json is working

--- src/ca/ece/ubc/cpen221/mp5/RestaurantDBTest.java
+++ src/ca/ece/ubc/cpen221/mp5/RestaurantDBTest.java
@@ -2,6 +2,11 @@ package ca.ece.ubc.cpen221.mp5;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import org.junit.Test;
 
 import ca.ece.ubc.cpen221.mp5.statlearning.Algorithms;
@@ -9,13 +14,18 @@ import ca.ece.ubc.cpen221.mp5.statlearning.Algorithms;
 public class RestaurantDBTest {
 
     @Test
-    public void test() {
+    public void test() throws IOException {
         RestaurantDB db = new RestaurantDB("restaurants.json", "reviews.json", "users.json");
         db.getRestaurantList();
         System.out.print(db.toString());
         
         String testString = Algorithms.convertClustersToJSON(Algorithms.kMeansClustering(3,db));
         System.out.println(testString);
+        
+        FileWriter out = new FileWriter("visualize/voronoi.json");
+        out.write(testString);
+        out.close();
+        
         
     }
 }
